10d3802608f2e739de74d0864cb0c406d2513f2d
ibro <sameoldlab@proton.me>
2024-01-18 11:35:41 +0100

    chore: :truck: move svelte site to examples/svelte

---
+++ examples/svelte/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { ConnectModal } from '@fractl-ui/evm'
+	import { http, createConfig, createStorage } from '@wagmi/core'
+	// import { injected, walletConnect, mock } from 'wagmi/connectors'
+	import { readable } from 'svelte/store'
+	import { mainnet, arbitrum } from '@wagmi/core/chains'
+	const storage = createStorage({ storage: localStorage })
+	const _config = createConfig({
+		chains: [mainnet, arbitrum],
+		storage,
+		transports: {
+			[mainnet.id]: http(),
+			[arbitrum.id]: http()
+		}
+		// connectors: [injected()],
+	})
+	const config = readable(_config, (set) => {
+		_config.subscribe(
+			(state) => state.status,
+			() => set(_config)
+		)
+	})
+</script>
+
+<ConnectModal config={$config}></ConnectModal>
+
+<style>
+</style>
