aca576c98b139e115279e984e71cd71c05007519
ibro <sameoldlab@proton.me>
2024-01-19 15:17:29 +0100

    add more readable formatting to truncated address

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,5 +1,5 @@
 export const truncate = (string: string, digits = 4) =>
-	string.substring(0, digits + 2) +
+	string.substring(0, digits + 2).replace('x', '×') +
 	'•••' +
 	string.substring(string.length - digits)
 
