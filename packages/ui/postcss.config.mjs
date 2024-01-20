c0c89c99b62e0d52e9528568f20ed810f5854f53
ibro <sameoldlab@proton.me>
2024-01-20 10:40:09 +0100

    build: compile css to support browser's without nesting
    locking browser versions with OS updates is the most asinine thing ever and could only be done by some deeply unserious corporation -_-

---
+++ packages/ui/postcss.config.mjs
@@ -0,0 +1,17 @@
+import postcssPresetEnv from 'postcss-preset-env'
+// const postcss = require('postcss');
+// const postcssPresetEnv = require('postcss-preset-env');
+
+const config = {
+	plugins: [
+		/** @type {import('postcss-preset-env').pluginOptions} */
+		postcssPresetEnv({
+			features: {
+				'nesting-rules': true
+			},
+			minimumVendorImplementations: 3
+		})
+	]
+}
+
+export default config
