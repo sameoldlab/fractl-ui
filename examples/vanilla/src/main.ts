2e6325ad27a5dc5cd0aac209e1c52557e3faab3f
ibro <sameoldlab@proton.me>
2024-02-06 06:05:31 +0100

    feat(examples): auto reconnect wallet in vanilla

--- examples/vanilla/src/main.ts
+++ examples/vanilla/src/main.ts
@@ -2,12 +2,14 @@ import './style.css'
 import '@fractl-ui/ui'
 import { addEvmConnection } from '@fractl-ui/evm'
 import { config as wagmiConfig } from './lib/wagmiConfig'
+import { reconnect } from '@wagmi/core'
 
 // const { status } = config.state
 // $: console.log('APP: ', config.state.status())
 const config = addEvmConnection(wagmiConfig)
 document.querySelector('fractl-modal')!.config = config //addEvmConnection(wagmiConfig)
 
+reconnect(wagmiConfig, { connectors: wagmiConfig.connectors })
 const log = document.querySelector('#logStatus')
 log?.addEventListener('click', () => {
 	console.log(config.state.status)
