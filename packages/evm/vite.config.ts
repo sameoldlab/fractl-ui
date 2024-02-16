8a90feceaef7950fffc47875f26b0bff479cdfaa
ibro <sameoldlab@proton.me>
2024-02-16 18:15:10 +0100

    evm: emit types with vite-plugin-dts

--- packages/evm/vite.config.ts
+++ packages/evm/vite.config.ts
@@ -1,8 +1,16 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+	plugins: [
+		dts({
+			entryRoot: 'src',
+			outDir: 'dist'
+		})
+	],
 	build: {
+		sourcemap: true,
 		lib: {
 			entry: resolve(__dirname, './src/index.ts'),
 			name: '@fractl-ui/evm',
