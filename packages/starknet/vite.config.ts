08f1bd0195382e9a271a1ddc5b0d66b3f0cf3739
ibro <sameoldlab@proton.me>
2024-02-16 17:46:24 +0100

    starknet: remove svelte ui code
    Signed-off-by: ibro <sameoldlab@proton.me>

--- packages/starknet/vite.config.ts
+++ packages/starknet/vite.config.ts
@@ -1,25 +1,20 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		svelte(),
 		dts({
-			entryRoot: 'src/lib',
-			outDir: 'dist/types'
+			entryRoot: 'src',
+			outDir: 'dist'
 		})
 	],
 	build: {
 		sourcemap: true,
 		lib: {
-			entry: resolve(__dirname, './src/lib/index.ts'),
+			entry: resolve(__dirname, './src/index.ts'),
 			name: '@fractl-ui/starknet', //should be able to convert to some general cairo version
-			formats: ['es'],
 			fileName: 'index'
 		},
-		target: 'esnext'
 	}
 })
