f7171c43ed668170f4ebf1b2bd395cc2d1f26101
ibro <sameoldlab@proton.me>
2024-05-03 23:31:07 +0100

    wrap evm and starknet connectors

---
+++ site/src/lib/connect/starknet.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { addStarknetConnection } from '@fractl-ui/starknet'
+	import 'fractl-ui'
+	const config = addStarknetConnection()
+</script>
+
+<fractl-modal {config} btnClass="button-85" />
