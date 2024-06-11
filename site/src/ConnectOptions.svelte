3c539a2c3c85b851dc3e03032fdda077d8b0498d
ibro <sameoldlab@proton.me>
2024-06-11 15:02:57 +0100

    create UI to adjust modal options

--- site/src/ConnectOptions.svelte
+++ site/src/ConnectOptions.svelte
@@ -3,6 +3,7 @@
 	// import './app.css'
 	import Evm from './lib/connect/evm.svelte'
 	import Starknet from './lib/connect/starknet.svelte'
+	import ConnectPanel from './ConnectPanel.svelte'
 
 	export let ecosystem: string
 	// export let theme = ""
@@ -10,7 +11,7 @@
 
 <div
 	style="
-			display: flex; 
+			display: flex;
 			gap: .25em;
 			font-family: Arial, Helvetica, sans-serif;
 			font-weight: 500;
@@ -24,22 +25,27 @@
 		class:selected={ecosystem === 'evm'}
 		on:click={() => {
 			ecosystem = 'evm'
-		}}>EVM</button
+		}}><h2>EVM</h2></button
 	>
 	<button
 		class="menu-option btn-reset"
 		class:selected={ecosystem === 'starknet'}
 		on:click={() => {
 			ecosystem = 'starknet'
-		}}>Starknet</button
+		}}><h2>Starknet</h2></button
 	>
 </div>
 <div class="preview">
-	{#if ecosystem === 'evm'}
-		<Evm class="connect" />
-	{:else if ecosystem === 'starknet'}
-		<Starknet class="connect" />
-	{/if}
+	<div class="filler">
+		{#if ecosystem === 'evm'}
+			<Evm class="connect" />
+		{:else if ecosystem === 'starknet'}
+			<Starknet class="connect" />
+		{/if}
+	</div>
+	<div class="settings">
+		<ConnectPanel />
+	</div>
 </div>
 
 <style>
@@ -48,11 +54,10 @@
 		min-width: 0;
 	}
 	.preview {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		/* margin: auto; */
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 		margin-block: 0.25em;
+		margin-block-end: 1em;
 		width: min(1000px, 100%);
 		border: 2px solid rgba(76 76 76 / 0.4);
 		border-radius: 0.5em;
@@ -63,7 +68,19 @@
 		);
 		background-size: 1.5em 1.5em;
 		/* opacity: 0.2; */
-		aspect-ratio: 3 / 1;
+		aspect-ratio: 4 / 3;
+	}
+	.settings {
+		display: flex;
+		align-items: stretch;
+		justify-content: stretch;
+	}
+
+	.filler {
+		min-height: 16em;
+		display: grid;
+		align-items: center;
+		justify-content: center;
 	}
 	.btn-reset {
 		background: none;
@@ -71,15 +88,22 @@
 		font-size: inherit;
 		border: none;
 		padding: 0;
+
+		& > * {
+			font-size: inherit;
+			font-weight: inherit;
+			padding: inherit;
+			margin: inherit;
+		}
 	}
 	.menu-option {
 		cursor: pointer;
 		font-size: 96%;
 		opacity: 0.7;
-		padding: 0.5em 1em;
+		padding: 0.125em .75em;
 		transition: background 200ms ease;
 		position: relative;
-		border-radius: 8px;
+		border-radius: 4px;
 	}
 	.menu-option.selected {
 		font-weight: 600;
