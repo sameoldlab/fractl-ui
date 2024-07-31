ed4d08e11b01012ba1512f23a9cc194e9dda9d63
ibro <sameoldlab@proton.me>
2024-07-31 10:49:49 +0100

    export connectModal as function

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,23 @@
 import ConnectModal from './ConnectModal/ConnectModal.svelte'
 import AccountModal from './AccountModal.svelte'
 import FractlModal from './FractlModal.svelte'
+import type { Config } from '@fractl-ui/types'
 
+export const create = async <C extends object>(config: Promise<Config<C>>) => {
+	let _config = await Promise.resolve(config)
+
+	return () => {
+		const getTarget = () =>  document.body
+		
+		return new Promise((resolve, reject) => {
+			new ConnectModal({
+				target: getTarget(),
+				props: {
+					config: _config,
+					state: _config.state
+				}
+			})
+		})
+	}
+}
 export { ConnectModal, AccountModal, FractlModal }
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,3 @@
-export { FractlModal } from './components/index.js'
+export { create } from './components/index.js'
 // export {open connect modal}
 // export T&C text prop
