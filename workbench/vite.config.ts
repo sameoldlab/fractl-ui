bc479dad12a16f2fbc8984a1e6f0a75b5087f53c
ibro <sameoldlab@proton.me>
2024-08-09 13:32:21 +0100

    setup testing-library

--- workbench/vite.config.ts
+++ workbench/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelteTesting } from '@testing-library/svelte/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()]
+	plugins: [svelte(), svelteTesting()],
+	test: {
+		include: ["src/**/*.{test,test.svelte}.{js,ts}"],
+		environment: "jsdom",
+		includeSource: ["src/**/*.{js,ts,svelte}"],
+		setupFiles: ["./vitest-setup.ts"],
+	}
 })
